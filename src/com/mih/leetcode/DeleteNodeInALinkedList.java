29a7deb1db83827ef710abc754aa4c849c86ae61
meislzhua <444811296@qq.com>
2015-11-04 23:11:02 +0800

    finish Delete Node in a Linked List

---
+++ src/com/mih/leetcode/DeleteNodeInALinkedList.java
@@ -0,0 +1,35 @@
+package com.mih.leetcode;
+
+/**
+ * Created by meislzhua on 15/11/4.
+ */
+public class DeleteNodeInALinkedList {
+	public void deleteNode(ListNode node) {
+		//normal
+		//83 / 83 test cases passed.1 ms 5.02%
+		ListNode nextNode = node.next;
+		if(node == null || nextNode == null) return;
+		while(nextNode.next != null){
+			node.val = nextNode.val;
+			node = nextNode;
+			nextNode = nextNode.next;
+		}
+		node.val = nextNode.val;
+		node.next = null;
+	}
+
+	public static void main(String[] args) {
+		ListNode listNode = new ListNode(1);
+		listNode.next =new ListNode(2);
+		listNode.next.next =new ListNode(3);
+		listNode.next.next.next =new ListNode(4);
+		listNode.next.next.next.next =new ListNode(5);
+		listNode.next.next.next.next.next =new ListNode(6);
+
+		new  DeleteNodeInALinkedList().deleteNode(listNode.next);
+		while(listNode !=null){
+			System.out.println(listNode.val);
+			listNode = listNode.next;
+		}
+	}
+}
---
+++ src/com/mih/leetcode/ListNode.java
@@ -0,0 +1,10 @@
+package com.mih.leetcode;
+
+/**
+ * Created by meislzhua on 15/11/4.
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
